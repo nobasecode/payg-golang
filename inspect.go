0d5084292dfede8e177d5c1d8a7726985295f11b
Laabid <38099909+nobasecode@users.noreply.github.com>
2018-05-29 13:17:52 +0000

    Structured Results

--- inspect.go
+++ inspect.go
@@ -63,9 +63,7 @@ func inspect() [][]string {
 	var container []string
         
 	out, err := exec.Command("sh","-c","docker inspect --format='{{.State.Running}}|{{.Name}}|{{.State.StartedAt}}|{{.State.FinishedAt}}|{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -qa)").Output()  
-	      if err != nil {
-                    log.Fatal(err)
-             }
+	      if err != nil {log.Fatal(err)}
  	
 	s := strings.Split(string(out), "\n")
 	
@@ -179,7 +177,8 @@ func pay_as_go(containers [][]string) {
 	c_file := file_to_c("watchlist")
     //fmt.Println(c_file)
     all := ""
-    
+    a := "\n"
+
     for i := range containers {
         
         running , err := strconv.ParseBool(containers[i][1])
@@ -195,7 +194,9 @@ func pay_as_go(containers [][]string) {
                     
                     //fmt.Println(diff_date_now(s_date(c_file[j][2])))
                     
-                    t := time.Now()                    
+                    t := time.Now()
+                    a = a+"\033[32m"+containers[i][0]+"|"+strconv.Itoa(new_time)+"|"+t.Format("2006-01-02 15:04:05")+"\n"+"\033[39m"                  
+                    
                     all = all+containers[i][0]+"|"+strconv.Itoa(new_time)+"|"+t.Format("2006-01-02 15:04:05")+"\n"
                 }
             }
@@ -210,6 +211,8 @@ func pay_as_go(containers [][]string) {
                     //fmt.Println(diff_date(s_date(containers[i][3]),s_date(c_file[j][2])))
                     
                     t := time.Now()
+                    a = a+"\033[31m"+containers[i][0]+"|"+strconv.Itoa(new_time)+"|"+t.Format("2006-01-02 15:04:05")+"\n"+"\033[39m"
+                    
                     all = all+containers[i][0]+"|"+strconv.Itoa(new_time)+"|"+t.Format("2006-01-02 15:04:05")+"\n"
 
                 }
@@ -221,8 +224,15 @@ func pay_as_go(containers [][]string) {
     err := ioutil.WriteFile("watchlist", []byte(all), 0666)
     if err != nil {log.Fatal(err)}
 
-    fmt.Println("\n"+all)
+    //fmt.Println("\n"+all)
 
+	output, err := exec.Command("echo", "-e",a).CombinedOutput()
+	if err != nil {
+	  os.Stderr.WriteString(err.Error())
+	}
+	fmt.Println(string(output)) 	
+
+	 
 }
 
 func main(){
@@ -230,13 +240,10 @@ func main(){
     c := cron.New()
     c.AddFunc("@every 2s", func() { 
         pay_as_go(inspect())
-        fmt.Println(inspect())
+        //fmt.Println(inspect())
      })
     c.Run()
     c.Start()
- 
-    
-	
-	
+
 
 }
